a95458f510ece277d255def27420b0a455955260
Taher Jerjissi <128802571+TahierJerr@users.noreply.github.com>
2024-06-14 15:41:39 +0200

    nice

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,24 +1,87 @@
-import { MailIcon, PhoneCallIcon } from 'lucide-react'
-import React from 'react'
+"use client"
 
-const ContactPage = () => {
+import { Time, getLocalTimeZone, today } from '@internationalized/date';
+import { Button } from '@nextui-org/button'
+import { Input, Textarea } from '@nextui-org/input'
+import { MailIcon, PhoneIcon } from 'lucide-react'
+
+const InschrijvenPage = () => {
+
+
+    const currentDay = today(getLocalTimeZone())
+
+    const time = new Time
+
+    console.log(time)
+
     return (
-        <div className='flex items-center justify-center flex-col max-w-screen-lg mx-auto'>
-            <div className='my-12'>
-                <h1 className='text-xl font-semibold text-center'>Neem contact met ons op</h1>
-                <div className='grid grid-cols-1 sm:grid-cols-2 gap-8 my-4'>
-                    <div className='bg-gray-300 rounded-md py-4 px-8 w-full flex items-center flex-col justify-center'>
-                        <p className='text-lg font-medium mb-4'>Wilt u ons bellen?</p>
-                        <a href="tel: 0597646049" className='rounded-lg bg-red-600 text-white px-4 py-2 flex items-center justify-center'><PhoneCallIcon size={20} className="mr-2" />0597 – 646049</a>
-                    </div>
-                    <div className='bg-gray-300 rounded-md py-4 px-8 w-full flex items-center flex-col justify-center'>
-                        <p className='text-lg font-medium mb-4'>Wilt u ons mailen?</p>
-                        <a href="mailto: info@vitasalis.nl" className='rounded-lg bg-red-600 text-white px-4 py-2 flex items-center justify-center'><MailIcon size={20} className='mr-2' />info@vitasalis.nl</a>
+        <div className='flex items-center justify-center flex-col max-w-screen-lg mx-auto sm:h-auto    '>
+            <div className='flex flex-col sm:flex-row justify-center items-center sm:items-start'>
+                <div className='sm:my-12 my-4 flex items-center justify-center flex-col gap-4 rounded-lg'>
+                    <h1 className='text-2xl font-semibold'>Contact</h1>
+                    <form
+                        className='flex flex-col justify-center gap-4 p-4 bg-gray-200 mx-4 rounded-lg'
+                    >
+                        <div className='flex items-center gap-2'>
+                            <div>
+                                <Input label='Voornaam' placeholder='Jan' labelPlacement='outside' isRequired />
+                            </div>
+                            <div>
+                                <Input label='Achternaam' placeholder='Modaal' labelPlacement='outside' isRequired />
+                            </div>
+                        </div>
+                        <div>
+                            <Input
+                                startContent={<MailIcon size={20} className='text-gray-400' />}
+                                type='email'
+                                label='E-mail'
+                                placeholder='Janmodaal@gmail.com'
+                                labelPlacement='outside'
+                                isRequired
+                            />
+                        </div>
+                        <div>
+                            <Input
+                                startContent={<PhoneIcon size={20} className='text-gray-400' />}
+                                type='tel'
+                                placeholder='06123456789'
+                                labelPlacement='outside'
+                                label='Telefoonnummer (optioneel)'
+                            />
+                        </div>
+                        <div>
+                            <Textarea 
+                            isRequired
+                            label='Bericht'
+                            placeholder='Typ hier je bericht'
+                            labelPlacement='outside'
+                            size='lg'
+                            />
+                        </div>
+                        
+                        <div className='mt-4'>
+                            <Button className=' text-lg flex items-center justify-center gap-2 px-4 py-2 bg-red-600 text-white w-full'>
+                                Bericht versturen
+                            </Button>
+                        </div>
+                    </form>
+                </div>
+                <div className='sm:my-12 my-4 sm:mx-0 mx-4 sm:max-w-56 mb-12'>
+                    <p className='text-xl font-medium mb-5'>Liever bellen?</p>
+                    <div className='bg-gray-200 p-4 rounded-lg'>
+                        <p>U kunt ons bereiken op maandag t/m vrijdag van 8:00 tot 18:00 of zaterdag van 12:00 tot 18:00</p>
+                        <p></p>
+                        <div className='flex flex-col my-2'>
+                            <p className='text-lg font-medium'>Telefoonnummer:</p>
+                            <a href='tel: 0597 – 646049' className=' cursor-pointer underline-offset-2 underline hover:text-red-600'>
+                                0597 646049
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default ContactPage
+export default InschrijvenPage
--- app/inschrijven/page.tsx
+++ app/inschrijven/page.tsx
@@ -6,7 +6,6 @@ import { DatePicker } from '@nextui-org/date-picker'
 import { Input } from '@nextui-org/input'
 import { Select, SelectItem } from '@nextui-org/select'
 import { MailIcon, PhoneIcon, CakeIcon } from 'lucide-react'
-import React from 'react'
 
 const InschrijvenPage = () => {
 
@@ -17,54 +16,94 @@ const InschrijvenPage = () => {
         { label: 'Zeg ik liever niet'}
     ]
 
-    
-  return (
-    <div className='flex items-center justify-center flex-col max-w-screen-lg mx-auto overflow-hidden'>
-        <div className='flex flex-col sm:flex-row justify-center items-center'>
-        <div className='my-12 flex items-center justify-center flex-col gap-4 rounded-lg'>
-            <h1 className='text-2xl font-semibold'>Inscrhijven</h1>
-            <form className='flex flex-col justify-center gap-4 p-4 bg-gray-200 mx-4 rounded-lg'>
-                <div className='flex items-center gap-2'>
-                    <div>
-                    <Input label='Voornaam' placeholder='Jan' labelPlacement='outside' isRequired />
+    return (
+        <div className='flex items-center justify-center flex-col max-w-screen-lg mx-auto sm:h-auto    '>
+            <div className='flex flex-col sm:flex-row justify-center items-center sm:items-start'>
+                <div className='sm:my-12 my-4 flex items-center justify-center flex-col gap-4 rounded-lg'>
+                    <h1 className='text-2xl font-semibold'>Inscrhijven</h1>
+                    <form
+                        className='flex flex-col justify-center gap-4 p-4 bg-gray-200 mx-4 rounded-lg'
+                    >
+                        <div className='flex items-center gap-2'>
+                            <div>
+                                <Input label='Voornaam' placeholder='Jan' labelPlacement='outside' isRequired />
+                            </div>
+                            <div>
+                                <Input label='Achternaam' placeholder='Modaal' labelPlacement='outside' isRequired />
+                            </div>
+                        </div>
+                        <div>
+                            <Input
+                                className='w-full'
+                                label='BSN Nummer'
+                                placeholder='123456789'
+                                labelPlacement='outside'
+                                isRequired
+                                type='number'
+                            />
+                        </div>
+                        <div>
+                            <Select label='Geslacht' labelPlacement='outside' placeholder='Man' isRequired items={geslacht}>
+                                {(geslacht) => <SelectItem key={geslacht.label}>{geslacht.label}</SelectItem>}
+                            </Select>
+                        </div>
+                        <div>
+                            <Input
+                                startContent={<MailIcon size={20} className='text-gray-400' />}
+                                type='email'
+                                label='E-mail'
+                                placeholder='Janmodaal@gmail.com'
+                                labelPlacement='outside'
+                                isRequired
+                            />
+                        </div>
+                        <div>
+                            <Input
+                                startContent={<PhoneIcon size={20} className='text-gray-400' />}
+                                type='tel'
+                                placeholder='06123456789'
+                                labelPlacement='outside'
+                                label='Telefoonnummer'
+                                isRequired
+                            />
+                        </div>
+                        <div>
+                            <DatePicker
+                                showMonthAndYearPickers
+                                isRequired
+                                label='Geboortedatum'
+                                placeholderValue={today(getLocalTimeZone()).subtract({ years: 42 })}
+                                maxValue={today(getLocalTimeZone()).subtract({ years: 14 })}
+                                labelPlacement='outside'
+                                startContent={<CakeIcon size={20} />}
+                                className='w-full'
+                            />
+                        </div>
+                        <div className='mt-4'>
+                            <Button className=' text-lg flex items-center justify-center gap-2 px-4 py-2 bg-red-600 text-white w-full'>
+                                Inschrijven
+                            </Button>
+                        </div>
+                    </form>
+                </div>
+                <div className='sm:my-12 my-4 sm:mx-0 mx-4 sm:max-w-56 mb-12'>
+                    <p className='text-xl font-medium mb-6'>Lukt het niet?</p>
+                    <div className='bg-gray-200 p-4 rounded-lg'>
+                        <p>Neem contact met ons op of kom even langs en wij helpen je verder</p>
+                        <div className='flex flex-col my-2'>
+                            <p className='text-lg font-medium'>Contactinfo:</p>
+                            <a href='tel: 0597 – 646049' className=' cursor-pointer underline-offset-2 underline hover:text-red-600'>
+                                0597 646049
+                            </a>
+                            <a href='mailto: info@vitasalis.nl' className='cursor-pointer underline-offset-2 underline hover:text-red-600'>
+                                info@vitasalis.nl
+                            </a>
+                        </div>
                     </div>
-                    <div>
-                    <Input label='Achternaam' placeholder='Modaal' labelPlacement='outside' isRequired />
-                    </div>
                 </div>
-                <div>
-                    <Select label='Geslacht' labelPlacement='outside' placeholder='Man' isRequired items={geslacht}>
-                        {(geslacht) => <SelectItem key={geslacht.label}>{geslacht.label}</SelectItem>}
-                    </Select>
-                </div>
-                <div>
-                    <Input startContent={<MailIcon size={20} className='text-gray-400' />} type='email' label='E-mail' placeholder='Janmodaal@gmail.com' labelPlacement='outside' isRequired />
-                </div>
-                <div>
-                    <Input startContent={<PhoneIcon size={20} className='text-gray-400' />} type='tel' placeholder='06123456789' labelPlacement='outside' label='Telefoonnummer'  isRequired />
-                </div>
-                <div>
-                    <DatePicker showMonthAndYearPickers isRequired label="Geboortedatum" placeholderValue={today(getLocalTimeZone()).subtract({ years: 42 })}  maxValue={today(getLocalTimeZone()).subtract({ years: 14 })} labelPlacement='outside' startContent={<CakeIcon size={20} />} className='w-full' />
-                </div>
-                <div className='mt-4'>
-                    <Button className=' text-lg flex items-center justify-center gap-2 px-4 py-2 bg-red-600 text-white w-full'>Inschrijven</Button>
-                </div>
-            </form>
-        </div>
-        <div className='sm:my-12 my-4 sm:mx-0 mx-4  sm:max-w-56 '>
-            <p className='text-xl font-medium mb-6'>Lukt het niet?</p>
-            <div className='bg-gray-200 p-4 rounded-lg'>
-            <p>Neem contact met ons op of kom even langs en wij helpen je verder</p>
-            <div className='flex flex-col my-2'>
-            <p className='text-lg font-medium'>Contactinfo:</p>
-            <a href='tel: 0597 – 646049' className=' cursor-pointer underline-offset-2 underline hover:text-red-600'>0597 646049</a>
-            <a href="mailto: info@vitasalis.nl" className='cursor-pointer underline-offset-2 underline hover:text-red-600'>info@vitasalis.nl</a>
-            </div>
             </div>
         </div>
-        </div>
-    </div>
-  )
-}
+    );
+};
 
 export default InschrijvenPage
